a6b4d6d9678b05216deebf4255414e3410fd4089
andrew <bulatovandrew@gmail.com>
2026-04-07 22:01:40 +0300

    json5

--- main.py
+++ main.py
@@ -4,21 +4,16 @@ from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route
 from sys import modules
+from json5 import loads
 
 
 async def homepage(request):
     return JSONResponse({'hello': 'world'})
 
 
-async def do_call(request):
-    x = await request.json()
-    x = await call_json(x)
-    return JSONResponse(x)
-
-
 def api_help(method=None, **kwarggs):
     """
-    Возвращает список методов, ecли вызвана без параметров
+    Возвращает список методов ecли вызвана без параметров
     Либо информацию о методе, если его имя передано в параметр method
     """
     if method:
@@ -28,19 +23,34 @@ def api_help(method=None, **kwarggs):
     return lst
 
 
+async def do_call(request):
+    """
+    Вызов метода API
+    :param request: параметры JsonRPC
+    :return:
+    """
+    x = await request.body()
+    x = loads(x.decode())
+    x = await call_json(x)
+    return JSONResponse(x)
+
+
 def init_app(cfg_file):
-    if not Config.initialized:
-        cfg = Config(cfg_file, can_include=["API"])
-        for x in cfg.API.keys():
-            fnc = cfg.API[x]
-            typ = fnc.get("type", "db")
-            if typ == "db":
+    """
+    Инициализация приложения
+    настройка API. API может лежать в инклуде
+    """
+    cfg = Config(cfg_file, can_include=["API"])
+    for x in cfg.API.keys():
+        fnc = cfg.API[x]
+        match fnc.get("type", "db"):
+            case "db":
                 register_rpc(x, f"\JSON {fnc['function']}(JSONB)", help=fnc.get('help'))
-            elif typ == "self":
+            case "self":
                 mdl = modules[__name__]
                 fn = getattr(mdl, fnc['function'])
                 register_rpc(x, fn, help = fnc.get('help') )
-            else:
+            case _:
                 register_rpc(x, fnc["function"], help = fnc.get('help'))
 
     return Starlette(debug=True, routes=[
@@ -54,4 +64,4 @@ app = init_app("config.yaml")
 
 if __name__=="__main__":
     import uvicorn
-    uvicorn.run("main:app", port=5000, host="0.0.0.0")
+    uvicorn.run(app, port=5000, host="0.0.0.0")
